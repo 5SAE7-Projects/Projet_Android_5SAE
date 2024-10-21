f56b052803a4d8061f4ea6891d361d7f6c9732e9
houssem-eddinjallouli <houssem-eddin.jallouli@esprit.tn>
2024-10-21 20:46:59 +0100

    new project with roomDatabase

---
+++ EspritClubs/app/src/main/java/tn/esprit/espritclubs/database/AppDatabase.java
@@ -0,0 +1,36 @@
+package tn.esprit.espritclubs.database;
+
+import android.content.Context;
+
+import androidx.room.*;
+
+import java.util.concurrent.*;
+
+import tn.esprit.espritclubs.dao.UserDao;
+import tn.esprit.espritclubs.entities.User;
+import tn.esprit.espritclubs.utils.Converters;
+
+@Database(entities = {User.class}, version = 1, exportSchema = false)
+@TypeConverters({Converters.class})
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract UserDao userDao();
+    private static volatile AppDatabase INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    public static AppDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    AppDatabase.class, "clubs_database")
+                            .allowMainThreadQueries()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+}
